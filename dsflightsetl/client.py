c33033486646e8a76506a1dd85c0c196a475c4c2
ywatanabe <yu.w.tennis@gmail.com>
2025-04-15 18:23:50 +0900

    chore: Compact filtering

--- dsflightsetl/client.py
+++ dsflightsetl/client.py
@@ -1,4 +1,4 @@
-""" Module orchestrating all client side tasks """
+"""Module orchestrating all client side tasks"""
 
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
@@ -6,7 +6,7 @@ from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
 from dsflightsetl.airport import AirportLocation, AirportCsvPolicies
 from dsflightsetl.args import parse_args
 
-AIRPORT_CSV_PATH = 'gs://dsongcp-452504-cf-staging/bts/airport.csv'
+AIRPORT_CSV_PATH = "gs://dsongcp-452504-cf-staging/bts/airport.csv"
 
 
 def run(argv: list[str], save_main_sessions: bool = True) -> None:
@@ -21,19 +21,23 @@ def run(argv: list[str], save_main_sessions: bool = True) -> None:
     options.view_as(SetupOptions).save_main_session = save_main_sessions
 
     with beam.Pipeline(options=options) as pipeline:
-        airports = (pipeline
-                    | beam.io.ReadFromText(AIRPORT_CSV_PATH)
-                    | beam.Filter(lambda line: not AirportCsvPolicies.is_header(line))
-                    | beam.Filter(AirportCsvPolicies.is_us_airport)
-                    | beam.Map(lambda line: AirportLocation.of(line))  # pylint: disable=unnecessary-lambda
-                    )
+        airports = (
+            pipeline
+            | beam.io.ReadFromText(AIRPORT_CSV_PATH)
+            | beam.Filter(
+                lambda line: not AirportCsvPolicies.is_header(line)
+                and AirportCsvPolicies.is_us_airport
+            )
+            | beam.Map(lambda line: AirportLocation.of(line))
+        )
 
-        _ = (airports
-             | beam.Map(lambda airport: airport.to_csv())  # pylint: disable=unnecessary-lambda
-             | beam.io.WriteToText(
-                 file_path_prefix="gs://dsongcp-452504-cf-staging/tmp/extracted_airports",
-                 file_name_suffix=".csv"
-             )
-             )
+        _ = (
+            airports
+            | beam.Map(lambda airport: airport.to_csv())
+            | beam.io.WriteToText(
+                file_path_prefix="gs://dsongcp-452504-cf-staging/tmp/extracted_airports",
+                file_name_suffix=".csv",
+            )
+        )
 
     pipeline.run()
